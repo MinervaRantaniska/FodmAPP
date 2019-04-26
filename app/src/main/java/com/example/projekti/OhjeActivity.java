eee8ab889693ae7e9eaed70d5361135a8cc4338c
MinervaRantaniska <49865602+MinervaRantaniska@users.noreply.github.com>
2019-04-26 10:02:38 +0300

    Update OhjeActivity.java

--- app/src/main/java/com/example/projekti/OhjeActivity.java
+++ app/src/main/java/com/example/projekti/OhjeActivity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 public class OhjeActivity extends AppCompatActivity {
 
     TextView ohjeText;
-    private Button takaisinButton;
+    //private Button takaisinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +20,10 @@ public class OhjeActivity extends AppCompatActivity {
         //Intent intent = getIntent();
         ohjeText = (TextView) findViewById(R.id.OhjeKentta);
         ohjeText.setText("FodmAPP- sovelluksen käytöstä:\n\nSovellus on avuksi sinulle, joka haluat noudattaa FODMAP-ruokavaliota.\n\nHaku-valikostan voit etsiä FODMAP-hiilihydraatteihin kuuluvia ruoka-aineita suomen kielellä ja lisätä omaan listaan.\n\nOmassa listassa olevat ruoka-aineet voit merkitä liikennevaloväreillä itsellesi sopivuuden mukaan.\nPäiväkirja osioon voit tehdä omia merkintöjä. ");
-        takaisinButton = (Button) findViewById(R.id.takaisin);
+        //takaisinButton = (Button) findViewById(R.id.takaisin);
     }
-    public void takaisin(View v){
+    /*public void takaisin(View v){
         Intent takaisinButton = new Intent(this, MainActivity.class);
         startActivity(takaisinButton);
-    }
+    }*/
 }
